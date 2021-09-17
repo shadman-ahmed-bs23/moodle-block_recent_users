602264c175813e0b1939c14494499ec956017179
Shadman <shadman.chowdhury@brainstation23.com>
2021-09-17 17:11:04 +0600

    Add inital skeleton of block plugin

---
+++ block_recent_users.php
@@ -0,0 +1,44 @@
+<?php
+// This file is part of Moodle - https://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
+
+/**
+ * Recent users block.
+ *
+ * @package    block_recent_users
+ * @copyright  2021 Shadman Ahmed
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+class block_recent_users extends block_base {
+
+    function init() {
+        $this->title = get_string('pluginname', 'block_recent_users');
+    }
+
+    function get_content() {
+        if ($this->content !== NULL) {
+            return $this->content;
+        }
+
+        $this->content = new stdClass();
+        $this->content->text = "this is the text";
+        $this->content->footer= "This is the footer";
+
+        return $this->content;
+    }
+}
---
+++ lang/en/block_recent_users.php
@@ -0,0 +1,28 @@
+<?php
+// This file is part of Moodle - https://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'block_recent_users'.
+ *
+ * @package    block_recent_users
+ * @copyright  2021 Shadman Ahmed
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['recent_users:addinstance'] = 'Add a new recent users block';
+$string['recent_users:myaddinstance'] = 'Add a new recent users block to Dashboard';
+$string['recent_users'] = 'recent users block';
+$string['pluginname'] = 'Recent Users Block';
